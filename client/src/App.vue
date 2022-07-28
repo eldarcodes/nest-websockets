acf2b996907ed55893081d748d2cfc654d2bb7e6
mirzabekov00 <prog.eldar@gmail.com>
2022-07-28 20:57:26 +0300

    feat(client): display all messages

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,47 +1,29 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { io } from "socket.io-client";
+import { onBeforeMount, ref } from "vue";
+
+const socket = io("http://localhost:3000");
+const messages = ref([]);
+
+onBeforeMount(() => {
+  socket.emit("findAllMessages", {}, (response) => {
+    messages.value = response;
+  });
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="chat">
+    <div class="chat-container">
+      <div class="messages-container">
+        <div v-for="message in messages">
+          [{{ message.name }}]: {{ message.text }}
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+@import "./assets/base.css";
 </style>
