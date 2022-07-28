0017957cfebf4ab887ee064c83e8f8ecce712f01
mirzabekov00 <prog.eldar@gmail.com>
2022-07-28 20:51:21 +0300

    feat(messages): implement base logic

---
+++ server/src/messages/messages.gateway.ts
@@ -0,0 +1,54 @@
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  WebSocketServer,
+  ConnectedSocket,
+} from '@nestjs/websockets';
+import { MessagesService } from './messages.service';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { Socket, Server } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+})
+export class MessagesGateway {
+  constructor(private readonly messagesService: MessagesService) {}
+
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('createMessage')
+  async create(@MessageBody() createMessageDto: CreateMessageDto) {
+    const message = await this.messagesService.create(createMessageDto);
+
+    this.server.emit('message', message);
+
+    return message;
+  }
+
+  @SubscribeMessage('findAllMessages')
+  findAll() {
+    return this.messagesService.findAll();
+  }
+
+  @SubscribeMessage('join')
+  async joinRoom(
+    @MessageBody('name') name: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+    return this.messagesService.identify(name, client.id);
+  }
+
+  @SubscribeMessage('typing')
+  async typing(
+    @MessageBody('isTyping') isTyping: boolean,
+    @ConnectedSocket() client: Socket,
+  ) {
+    const clientName = await this.messagesService.getClientName(client.id);
+
+    client.broadcast.emit('typing', { name: clientName, isTyping });
+  }
+}
